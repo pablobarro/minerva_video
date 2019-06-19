eef8e8e0654707f45ef49ab6c6cb63a81beed91e
pablobarro <pablobarro.f@gmail.com>
2019-06-19 14:13:51 +0200

    Add files via upload

--- src/app/categoria_detalle/categoria.component.ts
+++ src/app/categoria_detalle/categoria.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Video } from '../video';
-import {Categoria} from '../categoria';
+import { Categoria } from '../categoria';
 import { VideoService } from '../services/detail.service';
 
 @Component({
@@ -14,31 +14,45 @@ import { VideoService } from '../services/detail.service';
 })
 
 export class CategoriaComponent implements OnInit {
-    public video: Video;
+    public videos: Video[];
     public categoria: Categoria;
-    
+    public categoriaVideo: {categoria: string;};
+
     constructor(
         private route: ActivatedRoute,
-        private videoService: VideoService,
+        private _videoService: VideoService,
         private location: Location
     ) {
         console.log(+this.route.snapshot.paramMap.get('id'));
     }
 
-    ngOnInit(): void {
-        this.getVideo();
+    ngOnInit() {
+        this.getVideos();
+        this.getCategoria();
+        //this.getCategoriaVideo();
+        this.categoriaVideo = {
+          categoria: this.route.snapshot.params.name
+          };
     }
+
     getCategoria(): void {
         const id = +this.route.snapshot.paramMap.get('id');
         // tslint:disable-next-line:no-shadowed-variable
-        this.videoService.getCategoria(id).subscribe(categoria => this.categoria = categoria);
+        this._videoService.getCategoria(id).subscribe(categoria => this.categoria = categoria);
     }
-    getVideo(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
-        // tslint:disable-next-line:no-shadowed-variable
-        this.videoService.getVideo(id).subscribe(video => this.video = video);
 
-    }
+    getVideos(): void {
+      this._videoService.getVideos().subscribe(videos => this.videos = videos);
+
+  }
+    getCategoriaVideo(){
+     // return this.videos.filter((videos) => videos.categoria === this.categoriaVideo);
+     /*this.categoriaVideo = {
+      categoria: this.route.snapshot.params.name
+      };
+      var filtroCategoria = this.categoriaVideo;
+      console.log(filtroCategoria);*/
+  }
     goBack(): void {
         this.location.back();
     }
