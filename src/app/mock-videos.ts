10d1a36d2fbd8dab66f8b8ba0c88b757c0ebd783
pablobarro <pablobarro.f@gmail.com>
2019-07-24 13:50:21 +0200

    Update mock-videos.ts

--- src/app/mock-videos.ts
+++ src/app/mock-videos.ts
@@ -17,8 +17,8 @@ export const VIDEOS: Video[] = [
         categoria: 'ciudades',
         // tslint:disable-next-line:max-line-length
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
-        poster: 'img/categorias/ciudades/videos/street.jpg',
-        src: 'videos/ciudades/Street_19627.mp4'
+        poster: 'https://cdn.pixabay.com/photo/2015/11/06/11/32/girl-1026246_960_720.jpg',
+        src: '//player.vimeo.com/video/321298091?title=0&portrait=0&byline=0&autoplay=1&loop=1'
     },
     {
         id: 3,
@@ -26,17 +26,63 @@ export const VIDEOS: Video[] = [
         categoria: 'ciudades',
         // tslint:disable-next-line:max-line-length
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
-        poster: 'img/categorias/ciudades/videos/walk.jpg',
-        src: 'videos/ciudades/Walking_21782.mp4'
+        poster: 'https://cdn.pixabay.com/photo/2017/08/12/19/01/walk-2635038_960_720.jpg',
+        src: '//player.vimeo.com/video/321298091?title=0&portrait=0&byline=0&autoplay=1&loop=1'
     },
     {
         id: 4,
-        name: 'Background',
+        name: 'Polygon',
         categoria: 'abstracto',
         // tslint:disable-next-line:max-line-length
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
-        poster: 'img/categorias/abstracto/videos/background.jpg',
-        src: 'videos/abstracto/TheBackground_22126.mp4'
+        poster: 'https://cdn.pixabay.com/photo/2017/03/25/18/06/color-2174066_960_720.png',
+        src: '//player.vimeo.com/video/183786490?title=0&portrait=0&byline=0&autoplay=1&loop=1'
+    },
+    {
+        id: 5,
+        name: 'Triangle',
+        categoria: 'abstracto',
+        // tslint:disable-next-line:max-line-length
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
+        poster: 'https://cdn.pixabay.com/photo/2016/05/24/14/54/polygon-1412485_960_720.png',
+        src: '//player.vimeo.com/video/318978190?title=0&portrait=0&byline=0&autoplay=1&loop=1'
+    },
+    {
+        id: 6,
+        name: 'Sphere',
+        categoria: 'abstracto',
+        // tslint:disable-next-line:max-line-length
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
+        poster: 'https://cdn.pixabay.com/photo/2017/03/25/18/06/color-2174066_960_720.png',
+        src: '//player.vimeo.com/video/183786490?title=0&portrait=0&byline=0&autoplay=1&loop=1'
+    },
+  
+    {
+        id: 7,
+        name: 'African',
+        categoria: 'personas',
+        // tslint:disable-next-line:max-line-length
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
+        poster: 'https://cdn.pixabay.com/photo/2017/04/03/03/58/african-2197414_960_720.jpg',
+        src: '//player.vimeo.com/video/158298505?title=0&portrait=0&byline=0&autoplay=1&loop=1'
+    },
+    {
+        id: 8,
+        name: 'Asian',
+        categoria: 'personas',
+        // tslint:disable-next-line:max-line-length
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
+        poster: 'https://cdn.pixabay.com/photo/2016/11/08/05/16/buddhist-1807518_960_720.jpg',
+        src: '//player.vimeo.com/video/136216213?title=0&portrait=0&byline=0&autoplay=1&loop=1'
+    },
+    {
+        id: 9,
+        name: 'European',
+        categoria: 'personas',
+        // tslint:disable-next-line:max-line-length
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus dapibus sem quis sodales. Donec at nibh rutrum lectus condimentum venenatis. Cras eu semper metus, vel bibendum ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent viverra quis elit sit amet vestibulum. Suspendisse potenti. Sed a neque auctor tortor luctus varius nec sagittis neque.',
+        poster: 'https://cdn.pixabay.com/photo/2014/07/25/08/48/alley-401540_960_720.jpg',
+        src: '//player.vimeo.com/video/182666664?title=0&portrait=0&byline=0&autoplay=1&loop=1'
     }
 
     ];
@@ -44,12 +90,18 @@ export const CATEGORIAS: Categoria[] = [
     {
         id: 1,
         name: 'ciudades',
-        imagen: 'img/categorias/ciudades/portada/portada.jpg'
+        imagen: 'https://cdn.pixabay.com/photo/2015/03/26/10/04/new-york-690868_960_720.jpg'
     },
     {
         id: 2,
         name: 'abstracto',
-        imagen: 'img/categorias/abstracto/portada/portada.jpg'
+        imagen: 'https://cdn.pixabay.com/photo/2017/07/03/20/17/abstract-2468874_960_720.jpg'
+
+    },
+    {
+        id: 3,
+        name: 'personas',
+        imagen: 'https://cdn.pixabay.com/photo/2014/05/03/00/56/summerfield-336672_960_720.jpg'
 
     }
 ];
